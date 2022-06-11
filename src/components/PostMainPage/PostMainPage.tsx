1cae204e2128f6ba968a5d6ae082e34897c6811e
Vladimir Drugov <wolffdrugov@yandex.ru>
2022-06-11 05:23:21 +0300

    no pin without pic

--- src/components/PostMainPage/PostMainPage.tsx
+++ src/components/PostMainPage/PostMainPage.tsx
@@ -1,4 +1,6 @@
+import axios, { AxiosResponse } from 'axios';
 import * as React from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
 import './PostMainPage.scss';
 
@@ -18,23 +20,47 @@ export interface Pin {
 }
 
 export default function PostMainPage({ img, title, author, reactions, _id }: Pin) {
-	return (
-		<Link to={`/pin/detailed/${_id}`} className="main-post__link">
-			<div className="main-post card">
-				<div className="main-post__pic ">
-					<img
-						className="main-post__pic-img card-img-top img-fluid"
-						loading="lazy"
-						src={typeof img === 'string' ? img : img.name}
-						alt="post"
-					/>
-				</div>
-				<div className="card-body">
-					<h5 className="main-post__title card-title">{title}</h5>
-					<p className="main-post__author card-text">{author} </p>
-					{reactions && <div className="main-post__reactions"></div>}
-				</div>
-			</div>
-		</Link>
-	);
+	const [elem, setElem] = useState<JSX.Element | null>(null);
+	const imgPromise: Promise<AxiosResponse<Pin>> | null =
+		typeof img === 'string' ? axios.get(img) : null;
+	const isDone = useRef(false);
+	if (!isDone.current) {
+		imgPromise
+			?.then(
+				() => {
+					setElem(
+						<Link to={`/pin/detailed/${_id}`} className="main-post__link">
+							<div className="main-post card">
+								<div className="main-post__pic ">
+									<img
+										className="main-post__pic-img card-img-top img-fluid"
+										loading="lazy"
+										src={typeof img === 'string' ? img : ''}
+										alt="post"
+									/>
+								</div>
+								<div className="card-body">
+									<h5 className="main-post__title card-title">{title}</h5>
+									<p className="main-post__author card-text">{author} </p>
+									{reactions && <div className="main-post__reactions"></div>}
+								</div>
+							</div>
+						</Link>
+					);
+					// console.log(img);
+				},
+				() => {
+					setElem(null);
+				}
+			)
+			.catch(() => {
+				setElem(null);
+			})
+
+			.finally(() => {
+				isDone.current = true;
+			});
+	}
+
+	return elem;
 }
