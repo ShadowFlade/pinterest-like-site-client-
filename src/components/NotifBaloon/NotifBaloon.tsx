66047fa1a0f6a266d252c39d41a1c1ebe80447c6
Vladimir Drugov <wolffdrugov@yandex.ru>
2022-06-14 12:08:00 +0300

    fixed badges style

--- src/components/NotifBaloon/NotifBaloon.tsx
+++ src/components/NotifBaloon/NotifBaloon.tsx
@@ -1,9 +1,13 @@
 import * as React from 'react';
 import './NotifBaloon.scss';
 export interface INotifBaloonProps {
-  notifCount: number;
+	notifCount: number;
 }
 
 export default function NotifBaloon({ notifCount }: INotifBaloonProps) {
-  return <span className="notif-baloon">{notifCount}</span>;
+	return (
+		<div className="notif-baloon">
+			<span className="notif-baloon__count">{notifCount}</span>
+		</div>
+	);
 }
