623f03c08e505ccfa7e999be13eb63aa1a296729
Vladimir Drugov <wolffdrugov@yandex.ru>
2022-07-17 00:41:14 +0300

    added postinstall script

--- src/utils/utils.d.ts
+++ src/utils/utils.d.ts
@@ -1,6 +1,6 @@
 declare function randomNumberBetween(min: number, max: number): number;
 declare function sample(array: any[]): any;
-declare const hideOnClickOutside: (e: MouseEvent, clickElement: null | HTMLElement, popElement: null | HTMLElement, cb?: (() => void) | undefined) => void;
+declare const hideOnClickOutside: (e: MouseEvent, clickElement: null | HTMLElement, popElement: null | HTMLElement, cb?: () => void) => void;
 declare const bindOutsideClickDetection: (clickElementName: string | HTMLElement, popElementName: string | HTMLElement, cb: () => void) => void;
 declare const calculateThePositionOfPopupElement: (anchorElement: HTMLElement | HTMLDivElement | null, popupElement: HTMLElement | null) => {
     offsetLeft: number;
