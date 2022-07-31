29d605aaf7a2c4c1e0c837f43431604da39caad2
Vladimir Drugov <wolffdrugov@yandex.ru>
2022-07-31 17:04:17 +0300

    we actually dont use cookies

--- src/components/CookiePrompt/CookiePrompts.tsx
+++ src/components/CookiePrompt/CookiePrompts.tsx
@@ -1,9 +1,27 @@
 import * as React from 'react';
+import { useRef, useState } from 'react';
 import DefButton from '../defButton/defButton';
+import ModalTransparentSmall from '../ModalTransparentSmall/ModalTransparentSmall';
 import './CookiePrompts.scss';
 export default function CookiePrompt() {
+	const [isTransparent, setIsTransparent] = useState(false);
+	const [target, setTarget] = useState<HTMLElement | null>(null);
+	const cookie = useRef<HTMLDivElement | null>(null);
+	const hide = () => {
+		setIsTransparent(false);
+	};
+	const removeCookies = () => {
+		cookie.current && cookie.current.remove();
+		setIsTransparent(false);
+	};
+	const fakeCookies = (e: React.MouseEvent) => {
+		const target = e.target as HTMLElement;
+		setTarget(target);
+		setIsTransparent(true);
+		setTimeout(removeCookies, 3000);
+	};
 	return (
-		<div className="cookie-prompt container-fluid">
+		<div className="cookie-prompt container-fluid" ref={cookie}>
 			<div className="cookie-prompt__inner row align-items-center py-3 px-3">
 				<div className="cookie-prompt__text col-8">
 					<p>
@@ -13,17 +31,22 @@ export default function CookiePrompt() {
 					</p>
 				</div>
 				<div className="cookie-prompt__buttons col-4 d-flex ">
-					<div className="cookie-prompt__button mx-1">
+					<div className="cookie-prompt__button mx-1" onClick={fakeCookies}>
 						<DefButton>Accept all cookies</DefButton>
 					</div>
-					<div className="cookie-prompt__button mx-1">
+					<div className="cookie-prompt__button mx-1" onClick={fakeCookies}>
 						<DefButton>Reject all cookies</DefButton>
 					</div>
-					<div className="cookie-prompt__button mx-1">
+					<div className="cookie-prompt__button mx-1" onClick={fakeCookies}>
 						<DefButton>Let me choose</DefButton>
 					</div>
 				</div>
 			</div>
+			{target && (
+				<ModalTransparentSmall clickElement={target} isVisible={isTransparent} hide={hide}>
+					<p>Haha!We don't actually use cookies!</p>
+				</ModalTransparentSmall>
+			)}
 		</div>
 	);
 }
